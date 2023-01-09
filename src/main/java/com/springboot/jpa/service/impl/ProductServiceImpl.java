2a17cc566734cdfcf37dd8d44c813b893432f75a
jwelyl <cork2586@naver.com>
2023-01-09 19:50:41 +0900

    230109 19:50 service layer

--- src/main/java/com/springboot/jpa/service/impl/ProductServiceImpl.java
+++ src/main/java/com/springboot/jpa/service/impl/ProductServiceImpl.java
@@ -1,2 +1,39 @@
-package com.springboot.jpa.service.impl;public class ProductServiceImpl {
+package com.springboot.jpa.service.impl;
+
+
+import com.springboot.jpa.data.dao.ProductDAO;
+import com.springboot.jpa.data.dto.ProductDto;
+import com.springboot.jpa.data.dto.ProductResponseDto;
+import com.springboot.jpa.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+    private final ProductDAO productDAO;
+
+    @Autowired
+    public ProductServiceImpl(ProductDAO productDAO) {
+        this.productDAO = productDAO;
+    }
+
+    @Override
+    public ProductResponseDto getProduct(Long number) {
+        return null;
+    }
+
+    @Override
+    public ProductResponseDto saveProduct(ProductDto productDto) {
+        return null;
+    }
+
+    @Override
+    public ProductResponseDto changeProductName(Long number, String name) throws Exception {
+        return null;
+    }
+
+    @Override
+    public void deleteProduct(Long number) throws Exception {
+
+    }
 }
