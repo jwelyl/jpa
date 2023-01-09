389ffd2a231bf1456fcba9b2dc39020a1674ddd9
jwelyl <cork2586@naver.com>
2023-01-10 02:26:41 +0900

    230110 Controller test

--- src/main/java/com/springboot/jpa/data/dto/ChangeProductNameDto.java
+++ src/main/java/com/springboot/jpa/data/dto/ChangeProductNameDto.java
@@ -1,2 +1,15 @@
-package com.springboot.jpa.data.dto;public class ChangeProductNameDto {
+package com.springboot.jpa.data.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class ChangeProductNameDto {
+    private Long number;
+    private String name;
 }
