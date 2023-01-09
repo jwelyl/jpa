f227e2106e6126dfd9cf0b2d7e112c972153ad26
jwelyl <cork2586@naver.com>
2023-01-09 20:25:06 +0900

    230109 20:24 ServiceImpl saveProduct

--- src/main/java/com/springboot/jpa/service/impl/ProductServiceImpl.java
+++ src/main/java/com/springboot/jpa/service/impl/ProductServiceImpl.java
@@ -9,6 +9,8 @@ import com.springboot.jpa.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+
 @Service
 public class ProductServiceImpl implements ProductService {
     private final ProductDAO productDAO;
@@ -33,7 +35,22 @@ public class ProductServiceImpl implements ProductService {
 
     @Override
     public ProductResponseDto saveProduct(ProductDto productDto) {
-        return null;
+        Product product = new Product();
+        product.setName(productDto.getName());
+        product.setPrice(productDto.getPrice());
+        product.setStock(productDto.getStock());
+        product.setCreatedAt(LocalDateTime.now());
+        product.setUpdatedAt(LocalDateTime.now());
+
+        Product savedProduct = productDAO.insertProduct(product);
+
+        ProductResponseDto productResponseDto = new ProductResponseDto();
+        productResponseDto.setNumber(savedProduct.getNumber());
+        productResponseDto.setName(savedProduct.getName());
+        productResponseDto.setPrice(savedProduct.getPrice());
+        productResponseDto.setStock(savedProduct.getStock());
+
+        return productResponseDto;
     }
 
     @Override
